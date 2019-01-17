ead8f61c7e3ba8bb99e13e3f30fa1ed68a480659
Кутякин Михаил <michakoutjakin@gmail.com>
2019-01-17 22:05:19 +0300

    copy from react base

---
+++ src/components/Add.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import {Add} from './components/Add'
+import {Comments} from './components/Comments'
+import './App.css'
+
+class App extends React.Component {
+    state = {
+        comments: null,
+        isLoading: false,
+    };
+
+    componentDidMount() {
+        this.setState({isLoading: true});
+        setTimeout(() => {
+            let comments = localStorage.getItem('comments');
+            comments = JSON.parse(comments) || [];
+
+            this.setState({
+                isLoading: false,
+                comments: comments.map((item) => {
+                    item.date = new Date(item.date);
+                    return item;
+                })
+            })
+        }, 1000)
+    }
+
+    handleAddComment = data => {
+        const nextComments = [data, ...this.state.comments];
+        this.updateComments(nextComments)
+    };
+    handleRemoveComment = removingComment => {
+        const nextComments = this.state.comments.filter((comment) => {
+            return comment.id !== removingComment.id;
+        });
+        this.updateComments(nextComments)
+    };
+    updateComments = (comments) => {
+        localStorage.setItem('comments', JSON.stringify(comments));
+        this.setState({comments: comments});
+    };
+
+    render() {
+        const {comments, isLoading} = this.state;
+
+        return (
+            <React.Fragment>
+                <Add onAddComment={this.handleAddComment}/>
+                <h3>Комментарии</h3>
+                {isLoading && <p>Загружаю...</p>}
+                {Array.isArray(comments) && <Comments
+                    onRemoveComment={this.handleRemoveComment}
+                    data={comments}
+                />}
+            </React.Fragment>
+        )
+    }
+}
+
+export default App
