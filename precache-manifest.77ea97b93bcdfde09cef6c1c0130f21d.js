a7b40ce89234c798c273e7c76ea80efc390d7150
Кутякин Михаил <michakoutjakin@gmail.com>
2019-01-20 00:56:30 +0300

    Updates

--- precache-manifest.77ea97b93bcdfde09cef6c1c0130f21d.js
+++ precache-manifest.77ea97b93bcdfde09cef6c1c0130f21d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/comments-widget-redux-app/static/js/runtime~main.62175636.js"
   },
   {
-    "revision": "3a26736914e24eb20506",
-    "url": "/comments-widget-redux-app/static/js/main.3a267369.chunk.js"
+    "revision": "1c3b9fc79f627050bb46",
+    "url": "/comments-widget-redux-app/static/js/main.1c3b9fc7.chunk.js"
   },
   {
-    "revision": "0524c24bf71dbdac6278",
-    "url": "/comments-widget-redux-app/static/js/1.0524c24b.chunk.js"
+    "revision": "ef2313be0ce063d0a04f",
+    "url": "/comments-widget-redux-app/static/js/1.ef2313be.chunk.js"
   },
   {
-    "revision": "3a26736914e24eb20506",
+    "revision": "1c3b9fc79f627050bb46",
     "url": "/comments-widget-redux-app/static/css/main.7662a179.chunk.css"
   },
   {
-    "revision": "4591f7d3c8cc01a0f8f3687e408979fa",
+    "revision": "5ce9e4d273deef5ea4a1b3a7fc8bcd11",
     "url": "/comments-widget-redux-app/index.html"
   }
 ];
