92895725e00a5df0cb3f32a24c994aa09c1dc18a
Кутякин Михаил <michakoutjakin@gmail.com>
2019-01-20 00:18:57 +0300

    Updates

---
+++ precache-manifest.cd1b87a742f476b7b7552bba15696603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "621756360267e992275f",
+    "url": "/comments-widget-redux-app/static/js/runtime~main.62175636.js"
+  },
+  {
+    "revision": "0b15af329b06e53cb0be",
+    "url": "/comments-widget-redux-app/static/js/main.0b15af32.chunk.js"
+  },
+  {
+    "revision": "78ffd3e4c7858d60e340",
+    "url": "/comments-widget-redux-app/static/js/1.78ffd3e4.chunk.js"
+  },
+  {
+    "revision": "0b15af329b06e53cb0be",
+    "url": "/comments-widget-redux-app/static/css/main.7662a179.chunk.css"
+  },
+  {
+    "revision": "18f83754a4b9b2fd433907f5be0bd2b2",
+    "url": "/comments-widget-redux-app/index.html"
+  }
+];
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { commentsReducer } from './comments';
-import { newCommentReducer } from './newComment';
-
-export const rootReducer = combineReducers({
-  comments: commentsReducer,
-  newComment: newCommentReducer,
-});
